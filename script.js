76a06a0db13180ad171c25ca208d228a63f02a91
SHASHWAT1223 <shashwattewari1223@gmail.com>
2022-04-01 01:42:33 +0530

    DONE

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather = {
+  apiKey: "d1d2d7086d1087b22c6aabbe9daf12d8",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").textContent = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon").textContent = temp + "°C";
+    document.querySelector(".description").textContent = description;
+    document.querySelector(".humidity").textContent =
+      "HUMIDITY: " + humidity + "%";
+    document.querySelector(".wind").textContent =
+      "WIND SPEED: " + speed + " km/hr";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("VARANASI");
